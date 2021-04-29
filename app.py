6b9b34e535606947d1370447277c17d880833156
Tak113 <takniques@gmail.com>
2021-04-28 21:56:45 -0700

    api in place

--- app.py
+++ app.py
@@ -14,23 +14,8 @@ from twitterscraper import query_tweets
 import datetime as dt
 import re
 
-# def load_flair():
-# 	return TextClassifier.load('en-sentiment')
-
-# def main():
-# 	tagger = load_flair()
-
-# 	st.title('Twitter Sentiment Analysis')
-# 	st.write('Single tweet classification')
-
-# 	input_sent = st.text_input('Tweet','I love Tokyo!')
-
-# 	s = Sentence(input_sent)
-# 	tagger.predict(s)
-# 	st.write('### Your sentence is ', str(s.labels))
-
-# if __name__ == '__main__':
-# 	main()
+# load TweetsGetter function from scrape_api.py
+from scrape_api import TweetsGetter
 
 
 st.title('Twitter Sentiment Analysis')
@@ -71,49 +56,68 @@ if input_tweet != '':
 # scrape
 
 st.subheader('Search twitter for query')
-st.write('`twitterscraper is pulling 0 query result now, changing to API-based query and still under dev.. Not working`')
+st.write('`Using twitter API instead of generic scraping method which mostly blocks by them. Pulling 10 tweets at once(API has a strict rate limits)`')
 
 
 # get user input
-query = st.text_input('Type `#query(hashtag)`:', '#')
+query = st.text_input('Type query`:', '#')
 
 # as long as the query is valid (not empty or equal to '#')...
 if query != '' and query != '#':
 	with st.spinner(f'Searching and analyzing {query}...'):
+		if __name__ == '__main__':
 		
-		# get english tweets from the past 4 weeks
-		tweets = query_tweets(query,
-			begindate = dt.date.today() - dt.timedelta(weeks=1),
-			lang = 'en')
-
+		    # get tweets by keyword
+		    getter = TweetsGetter.bySearch(query)
+		    
+		    # get tweets by user (screen_name)
+		    #getter = TweetsGetter.byUser('@realDonaldTrump')
+		    
+		    list_text = []
+		    list_id = []
+		    list_user_screenname = []
+		    list_created_at = []
+		    
+		    for tweet in getter.collect(total = 10): # total is number of tweets to get
+		        list_text.append(tweet['text'])
+		        list_id.append(tweet['id'])
+		        list_user_screenname.append(tweet['user']['screen_name'])
+		        list_created_at.append(tweet['created_at'])
+		
 		# initialize empty dataframe
 		tweet_data = pd.DataFrame({
-			'tweet': [],
-			'predicted-sentiment': []
+		    'tweet': [],
+		    'predicted-sentiment-value': [],
+		    'predicted-sentiment-score': [],
 		})
-
+		
 		# keep track of positive vs negative tweets
-		pos_vs_neg = {'POSITIVE': 0, 'NEGATIVE': 0}
-
+		pos_vs_neg = {'POSITIVE':0, 'NEGATIVE': 0}
+		
 		# add data for each tweet
-		for tweet in tweets:
-			# skip iteration if tweet is empty
-			if tweet.text in ('', ' '):
-				continue
-			# make predictions
-			sentence = Sentence(tweet.text)
-			tagger.predict(sentence)
-			# keep track of positive vs negative tweets
-			pos_vs_neg[sentiment.value] += 1 # value is either POSITIVE or NEGATIVE
-			# append new data
-			tweet_data = tweet_data.append({'tweet': tweet.text,
-				'predicted-sentiment': sentiment}, ignore_index=True)
+		for tweet in list_text:
+		    # skip iteration if tweet is empty
+		    if tweet in ('',' '):
+		        continue
+		    # make predictions
+		    sentence = Sentence(tweet)
+		    tagger.predict(sentence)
+		    # keep track of positive vs negative tweets
+		    pos_vs_neg[sentence.labels[0].value] += 1 #value is either POSITIVE or NEGATIVE
+		    # append new data
+		    tweet_data = tweet_data.append({'tweet': tweet,
+		                                    'predicted-sentiment-value': sentence.labels[0].value,
+		                                   'predicted-sentiment-score': sentence.labels[0].score}, ignore_index=True)
+		
+		# get positive rate
+		pos_rate = round(pos_vs_neg['POSITIVE']/(pos_vs_neg['NEGATIVE']+pos_vs_neg['POSITIVE'])*100,1)
+
 
 # show query data and sentiment if available
 try:
-	st.write(tweet_data)
+	st.write('check wide mode in appearance at settings to see entire tweets', tweet_data)
 	try:
-		st.write('Positive to negative tweet ratio:', pos_vs_neg['POSITIVE']/pos_vs_neg['NEGATIVE'])
+		st.write('POSITIVE tweet ratio:', pos_rate, '%')
 	except ZeroDivisionError: # if no negative tweets
 		st.write('All positive tweets')
 except NameError: # if no queries have been made yet
